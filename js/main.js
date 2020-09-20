52ff6490b08761d8811865265e4fa407be4559db
majahoberg <eaamhob@students.eaaa.dk>
2020-09-20 21:34:09 +0200

    tabbar + accordion

--- js/main.js
+++ js/main.js
@@ -29,11 +29,11 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 function userAuthenticated(user) {
   appendUserData(user);
-  showPage("home");
+  navigateTo("home");
 }
 
 function userNotAuthenticated() {
-  showPage("login");
+  navigateTo("login");
 
  // Firebase UI configuration (Inspiration fra Rasmus Cederdorff)
   const uiConfig = {
