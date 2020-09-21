23c2bcf48f2e15bf05ee8eca1b580bbc6d305171
majahoberg <eaamhob@students.eaaa.dk>
2020-09-21 10:05:50 +0200

    transform login buttons

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 function userAuthenticated(user) {
   appendUserData(user);
-  navigateTo("home");
+  navigateTo("interests");
 }
 
 function userNotAuthenticated() {
